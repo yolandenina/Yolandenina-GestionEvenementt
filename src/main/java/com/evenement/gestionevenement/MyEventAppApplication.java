48138b0d12fd0f97e6aae4e45299a53c9dc3e2da
Nganso Yolande <haileynina56@gmail.com>
2025-06-09 13:20:57 +0200

    first commit

---
+++ src/main/java/com/evenement/gestionevenement/MyEventAppApplication.java
@@ -0,0 +1,13 @@
+package com.evenement.gestionevenement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEventAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEventAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evenement/gestionevenement/MyEventAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evenement.gestionevenement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEventAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
